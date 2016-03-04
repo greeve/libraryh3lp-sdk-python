04b951e13eb4a8e88ed437884bba4e24829c6063
Eric Sessoms <eric.s.sessoms@gmail.com>
2016-03-04 08:56:03 -0500

    Expand template example to support interactive selection

--- examples/edit-template.py
+++ examples/edit-template.py
@@ -4,24 +4,40 @@
 # ----------------
 # Edit your FAQs templates in the console.  Because consoles rock.
 
+from cursesmenu import SelectionMenu
 from subprocess import call
 
+import argparse
 import lh3.api
 import os
 import sys
 import tempfile
 
-# Takes two command line arguments, FAQ ID and template name.
-faq_id, template_name = sys.argv[1:]
+parser = argparse.ArgumentParser(description = 'Edit a FAQ template')
+parser.add_argument('-f', '--faq', help = 'faq name')
+parser.add_argument('-t', '--template', help = 'template name')
 
-# Lazy typers don't need to add the .html extension on the template name.
-if template_name.find('.html') == -1:
-    template_name += '.html'
-template_name = unicode(template_name, 'utf-8')
-
+args = parser.parse_args()
 client = lh3.api.Client()
+
+faqs = client.all('faqs').get_list()
+if args.faq:
+    faq = (faq for faq in faqs if faq['name'] == faq_name).next()
+else:
+    selection = SelectionMenu.get_selection([f['name'] for f in faqs])
+    faq = faqs[selection]
+
+faq_id = faq['id']
 templates = client.one('faqs', faq_id).all('templates').get_list(params = {'format': 'json'})
-template = (template for template in templates if template['name'] == template_name).next()
+if args.template:
+    template_name = args.template
+    if template_name.find('.html') == -1:
+        template_name += '.html'
+    template_name = unicode(template_name, 'utf-8')
+    template = (template for template in templates if template['name'] == template_name).next()
+else:
+    selection = SelectionMenu.get_selection([t['name'] for t in templates])
+    template = templates[selection]
 
 EDITOR = os.environ.get('EDITOR', 'vim')
 
